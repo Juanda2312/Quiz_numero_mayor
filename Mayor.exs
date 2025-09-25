aec3a24ae79664b0213c1bb12d09e392690a854c
Juanda2312 <juand.tapierom@uqvirtual.edu.co>
2025-09-25 16:21:10 -0500

    Sin divide y venceras

--- Mayor.exs
+++ Mayor.exs
@@ -1,4 +1,4 @@
-#Utilizando divide y venceras
+#sin divide y venceras
 defmodule MayorLista do
   def main do
     n = [1,8,3,25,3,20]
@@ -6,21 +6,15 @@ defmodule MayorLista do
     |> IO.puts()
   end
 
-  def mayor([]), do: 0
+  def mayor([]),  do: 0
   def mayor([n]), do: n
-  def mayor(lista) do
-      mitad = div(length(lista),2)
-      {a,b} = Enum.split(lista,mitad)
-
-      mayor_a = mayor(a)
-      mayor_b = mayor(b)
-
-      if mayor_a >= mayor_b do
-        mayor_a
-      else
-        mayor_b
-      end
+  def mayor([head|tail]) do
+    mayor_tail = mayor(tail)
+    if head > mayor_tail do
+      head
+    else
+      mayor_tail
+    end
   end
 end
-
 MayorLista.main()
